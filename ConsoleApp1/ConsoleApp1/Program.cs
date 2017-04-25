3642779e562f9ecc4b471b9aa450fb30ec4dc18c
etang <etang@ainq.com>
2017-04-24 20:11:57 -0400

    change on dev

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    /// <summary>
+    /// This is a change made on dev branch
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
